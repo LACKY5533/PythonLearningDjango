e4ef6b7b19532848099b04415281bdc8da3f3582
LACKY5533 <atrashkov.nikita@mail.ru>
2025-05-27 15:26:22 +0300

    orm part2

--- MainApp/models.py
+++ MainApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-   name  = models.CharField(max_length=100)
-   brand = models.CharField(max_length=100)
+   name  = models.CharField('Название', max_length=100)
+   brand = models.CharField('Бренд', max_length=100)
    count = models.PositiveIntegerField() 
+   description = models.TextField('Описание', blank=True)
